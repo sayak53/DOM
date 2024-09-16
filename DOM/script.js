c01362dff21cd6bd6594f85e03e78a698adae2c6
sayak53 <sayakchakraborty928@gmail.com>
2024-09-16 18:59:33 +0530

    even handled by addEventListener

--- DOM/script.js
+++ DOM/script.js
@@ -1,7 +1,16 @@
-let ul = document.querySelector("ul")
-console.log(ul)
+// let ul = document.querySelector("ul")
+// console.log(ul)
 
-let li = document.createElement("li")
-li.innerText="nom"
+// let li = document.createElement("li")
+// li.innerText="nom"
 
-ul.append(li)
+// ul.append(li)
+
+document.getElementById('update').addEventListener('click',function(){
+    const inputField=document.getElementById('input-field')
+    const inputText = inputField.value
+
+    const p = document.getElementById('text')
+    p.innerText = inputText
+    inputField.value = " "
+})
