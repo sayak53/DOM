abdbf6955b1e7307fc6fe25084f7aea728759da4
sayak53 <sayakchakraborty928@gmail.com>
2024-09-25 20:22:00 +0530

    simple arrow function

--- js-revision/script.js
+++ js-revision/script.js
@@ -3,3 +3,16 @@ function add(num1,num2){
 }
 const sum =add(10,8)
 console.log(sum)
+
+const mul=(x,y) => x*y
+
+console.log(mul(8,5))
+
+const doMath=(a,b,c,d)=>{
+    const sum=a+b
+    const diff=c-d
+    const result=sum*diff
+    return result
+}
+
+const res=console.log(doMath(10,5,25,5))
