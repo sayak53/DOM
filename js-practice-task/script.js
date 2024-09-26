df670a539385c237aaf3abb6e50925e42eebb994
sayak53 <sayakchakraborty928@gmail.com>
2024-09-26 19:40:02 +0530

    problem 2 solved

--- js-practice-task/script.js
+++ js-practice-task/script.js
@@ -12,3 +12,11 @@ const friends=['sayak','rakib','rajesh','tornov','rohit']
 
 const result=FindEven(friends)
 console.log(result)
+
+const firstArray=[34,65,78,32,12,35]
+const secondArray=[31,34,2,165,98,65,34]
+
+const add=firstArray.concat(secondArray)
+console.log(add)
+
+console.log(Math.max(...add))
