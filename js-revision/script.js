41108e391cc21e5026f6f71288b269e9460a931c
sayak53 <sayakchakraborty928@gmail.com>
2024-09-26 21:25:34 +0530

    solved some quiz question

--- js-revision/script.js
+++ js-revision/script.js
@@ -31,3 +31,10 @@ const numbers=[12,4,7,5,4,6575,5754]
 
 const square=numbers.map(number => number*2)
 console.log(square)
+
+const nums = [1,2,3,4,5];
+let output = nums.filter(n => n%2);
+console.log(output);
+
+const [a, b] = [1,2,3,4,45,5]; 
+console.log(a+b);
