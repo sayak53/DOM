35ab5cc528d48d07d1cccde0f737f763932eef4f
sayak53 <sayakchakraborty928@gmail.com>
2024-09-28 12:03:33 +0530

    greetings function

--- js-revision/practice.js
+++ js-revision/practice.js
@@ -1,40 +1,47 @@
+// // const numbers=[1,3,5,7,9]
+// // let even=[]
+// // for(num of numbers){
+// //     if(num%2==1){
+// //         num=num+1
+// //         even.push(num)
+// //     }
+// // }
+// // console.log(even)
+
 // const numbers=[1,3,5,7,9]
-// let even=[]
-// for(num of numbers){
-//     if(num%2==1){
-//         num=num+1
-//         even.push(num)
-//     }
-// }
+
+// const even=numbers.map(num => num+=1)
+
 // console.log(even)
 
-const numbers=[1,3,5,7,9]
+// const array=[33,50,78,79,90,101,30]
 
-const even=numbers.map(num => num+=1)
+// const divisible=array.filter(num =>num%10==0)
 
-console.log(even)
+// console.log(divisible)
 
-const array=[33,50,78,79,90,101,30]
+// const constructor=[
+//     {name:'akil',age:26,position:'senior'},
+//     {name:'akifs',age:26,position:'junior'},
+//     {name:'asif',age:26,position:'senior'}
+// ]
+// const pos=constructor.filter(cons => cons.position == 'senior')
+// console.log(pos)
 
-const divisible=array.filter(num =>num%10==0)
+// const names=[
+//     {name:'akil',age:26,},
+//     {name:'akifs',age:29,},
+//     {name:'asif',age:35,}
+// ]
+// let age=0
+// for(const name of names){
+//     age += name.age
+// }
+// console.log(age)
 
-console.log(divisible)
-
-const constructor=[
-    {name:'akil',age:26,position:'senior'},
-    {name:'akifs',age:26,position:'junior'},
-    {name:'asif',age:26,position:'senior'}
-]
-const pos=constructor.filter(cons => cons.position == 'senior')
-console.log(pos)
-
-const names=[
-    {name:'akil',age:26,},
-    {name:'akifs',age:29,},
-    {name:'asif',age:35,}
-]
-let age=0
-for(const name of names){
-    age += name.age
+function greetings(greet,name){
+    return greet + ' ' + name
 }
-console.log(age)
+
+const res = greetings('Good Morning','Sayak')
+console.log(res)
