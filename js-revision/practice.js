4478aca4f7ea089f817175ed299a4be08f05e506
sayak53 <sayakchakraborty928@gmail.com>
2024-09-29 19:05:02 +0530

    removed comment

--- js-revision/practice.js
+++ js-revision/practice.js
@@ -46,10 +46,10 @@
 // const res = greetings('Good Morning','Sayak')
 // console.log(res)
 
-// function work(x, y = 4) {
-//     return x + y;
-//    }
-//    console.log(work(32));
+function work(x, y = 4) {
+    return x + y;
+   }
+   console.log(work(32));
 
 const sum =(p, q)=> {
      return p + q;
