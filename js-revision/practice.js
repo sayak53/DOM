a54af9f5c21f71c8b520719d6d5532a9c5bf1cdc
sayak53 <sayakchakraborty928@gmail.com>
2024-09-29 19:07:23 +0530

    almost done

--- js-revision/practice.js
+++ js-revision/practice.js
@@ -1,32 +1,32 @@
-// // const numbers=[1,3,5,7,9]
-// // let even=[]
-// // for(num of numbers){
-// //     if(num%2==1){
-// //         num=num+1
-// //         even.push(num)
-// //     }
-// // }
-// // console.log(even)
-
 // const numbers=[1,3,5,7,9]
-
-// const even=numbers.map(num => num+=1)
-
+// let even=[]
+// for(num of numbers){
+//     if(num%2==1){
+//         num=num+1
+//         even.push(num)
+//     }
+// }
 // console.log(even)
 
-// const array=[33,50,78,79,90,101,30]
+const numbers=[1,3,5,7,9]
 
-// const divisible=array.filter(num =>num%10==0)
+const even=numbers.map(num => num+=1)
 
-// console.log(divisible)
+console.log(even)
 
-// const constructor=[
-//     {name:'akil',age:26,position:'senior'},
-//     {name:'akifs',age:26,position:'junior'},
-//     {name:'asif',age:26,position:'senior'}
-// ]
-// const pos=constructor.filter(cons => cons.position == 'senior')
-// console.log(pos)
+const array=[33,50,78,79,90,101,30]
+
+const divisible=array.filter(num =>num%10==0)
+
+console.log(divisible)
+
+const constructor=[
+    {name:'akil',age:26,position:'senior'},
+    {name:'akifs',age:26,position:'junior'},
+    {name:'asif',age:26,position:'senior'}
+]
+const pos=constructor.filter(cons => cons.position == 'senior')
+console.log(pos)
 
 const names=[
     {name:'akil',age:26,},
